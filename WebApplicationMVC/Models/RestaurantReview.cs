69a783432c5cf06fe1ca698005c34dcf7192eef4
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-13 21:33:41 +0100

    Add data annotation for validation

--- WebApplicationMVC/Models/RestaurantReview.cs
+++ WebApplicationMVC/Models/RestaurantReview.cs
@@ -1,10 +1,17 @@
-﻿namespace WebApplicationMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVC.Models
 {
     public class RestaurantReview
     {
         public int Id { get; set; }
+        [Range(1,10)]
         public int Rating { get; set; }
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
+        [Display(Name = "User Name")]
+        [DisplayFormat(NullDisplayText = "anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId  { get; set; }
     }
