965f1f1eeb0279d98f4a9d917f3c7f7055644cc1
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-13 15:59:35 +0100

    Setup DB connection

--- WebApplicationMVC/Models/OdeToFoodDb.cs
+++ WebApplicationMVC/Models/OdeToFoodDb.cs
@@ -8,6 +8,10 @@ namespace WebApplicationMVC.Models
 {
     public class OdeToFoodDb : DbContext
     {
+        public OdeToFoodDb() : base("name=DefaultConnection")
+        {
+                
+        }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
     }
