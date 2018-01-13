d2f4c4342d032dcf92c6e709874ded32f35b77e4
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-13 22:03:57 +0100

    Remove Max words validation

--- WebApplicationMVC/Models/RestaurantReview.cs
+++ WebApplicationMVC/Models/RestaurantReview.cs
@@ -2,31 +2,6 @@
 
 namespace WebApplicationMVC.Models
 {
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        private readonly int _maxWords;
-
-        public MaxWordsAttribute(int maxWords)
-            : base("{0} has too many words.")
-        {
-             _maxWords = maxWords;
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
-    }
-
     public class RestaurantReview
     {
         public int Id { get; set; }
@@ -37,7 +12,6 @@ namespace WebApplicationMVC.Models
         public string Body { get; set; }
         [Display(Name = "User Name")]
         [DisplayFormat(NullDisplayText = "anonymous")]
-        [MaxWords(2)]
         public string ReviewerName { get; set; }
         public int RestaurantId  { get; set; }
     }
