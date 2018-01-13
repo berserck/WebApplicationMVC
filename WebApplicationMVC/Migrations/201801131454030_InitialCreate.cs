7a6f6293b06f66130427a128093b87dd9cff176d
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-13 16:09:08 +0100

    Remove unnecessary includes and namespaces

--- WebApplicationMVC/Migrations/201801131454030_InitialCreate.cs
+++ WebApplicationMVC/Migrations/201801131454030_InitialCreate.cs
@@ -1,8 +1,7 @@
 namespace WebApplicationMVC.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class InitialCreate : DbMigration
     {
         public override void Up()
