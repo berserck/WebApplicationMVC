bf8e52615067b6f2809e7edc4cd907eebff638c6
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-13 16:04:48 +0100

    Add automatic DB migration

---
+++ WebApplicationMVC/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace WebApplicationMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplicationMVC.Models.OdeToFoodDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "WebApplicationMVC.Models.OdeToFoodDb";
+        }
+
+        protected override void Seed(WebApplicationMVC.Models.OdeToFoodDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
