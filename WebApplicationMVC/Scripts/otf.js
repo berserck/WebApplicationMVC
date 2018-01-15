663b0ae88bfca8fea401bce966b445e7e32802b5
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-15 12:41:22 +0100

    Add ajax to the pager

--- WebApplicationMVC/Scripts/otf.js
+++ WebApplicationMVC/Scripts/otf.js
@@ -1,5 +1,5 @@
-﻿$(function() {
-    var ajaxFormSubmit = function() {
+﻿$(function () {
+    var ajaxFormSubmit = function () {
         var $form = $(this);
 
         var options = {
@@ -8,7 +8,7 @@
             data: $form.serialize()
         };
 
-        $.ajax(options).done(function(data) {
+        $.ajax(options).done(function (data) {
             var $target = $($form.attr("data-otf-target"));
             var $newHtml = $(data);
             $target.replaceWith($newHtml);
@@ -18,14 +18,14 @@
         return false;
     }
 
-    var submitAutocompleteForm = function(event, ui) {
+    var submitAutocompleteForm = function (event, ui) {
         var $input = $(this);
         $input.val(ui.item.label);
         var $form = $input.parents("form:first");
         $form.submit();
     }
 
-    var createAutocomplete = function() {
+    var createAutocomplete = function () {
         var $input = $(this);
         var options = {
             source: $input.attr("data-otf-autocomplete"),
@@ -34,7 +34,25 @@
         $input.autocomplete(options);
     }
 
+
+    var getPage = function () {
+        var $a = $(this);
+
+        var options = {
+            url: $a.attr("href"),
+            data: $("form").serialize(),
+            type: "get"
+        };
+
+        $.ajax(options).done(function(data) {
+            var target = $a.parents("div.pagedList").attr("data-otf-target");
+            $(target).replaceWith(data);
+        });
+        return false;
+    }
+
     $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
     $("input[data-otf-autocomplete]").each(createAutocomplete);
 
+    $(".body-content").on("click", ".pagedList a", getPage);
 });
