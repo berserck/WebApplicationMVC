ca18875ed07f1e661085602b53e4bd579cb877e8
Pedro Parracho <pedro.parracho@gmail.com>
2018-01-16 15:54:49 +0100

    Add interface for odeToFoodDb to allow for passing fake db in tests

--- WebApplicationMVC/Models/OdeToFoodDb.cs
+++ WebApplicationMVC/Models/OdeToFoodDb.cs
@@ -1,14 +1,28 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace WebApplicationMVC.Models
 {
-    public class OdeToFoodDb : DbContext
+
+    public interface IOdeToFoodDb : IDisposable
+    {
+        IQueryable<T> Query<T>() where T : class;
+    }
+
+    public class OdeToFoodDb : DbContext, IOdeToFoodDb
     {
         public OdeToFoodDb() : base("name=DefaultConnection")
         {
-                
+
         }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
+
+        IQueryable<T> IOdeToFoodDb.Query<T>()
+        {
+            return Set<T>();
+        }
     }
 }
